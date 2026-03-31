52a95b29f86bf5e3e0df2ca6644cd4da99ffae59
Sein <forsein1120@gmail.com>
2026-03-31 23:52:22 +0900

    Improve robots.txt for SEO optimization

--- app/robots.ts
+++ app/robots.ts
@@ -6,8 +6,18 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
+
+        // 불필요한 페이지 크롤링 방지
+        disallow: [
+          "/api/",
+          "/_next/",
+        ],
       },
     ],
+
     sitemap: "https://www.cocodoco.fun/sitemap.xml",
+
+    // 선택: 크롤러 기본 호스트 지정
+    host: "https://www.cocodoco.fun",
   };
 }
